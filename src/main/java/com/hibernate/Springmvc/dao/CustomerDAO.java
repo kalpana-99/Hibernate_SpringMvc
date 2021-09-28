a9fc967358c07872e63efda7607c61f7d0cdce96
kalpana <kalpana.ithagoni@wavelabs.ai>
2021-09-28 12:39:46 +0530

    initialcommit

---
+++ src/main/java/com/hibernate/Springmvc/dao/CustomerDAO.java
@@ -0,0 +1,18 @@
+package com.hibernate.Springmvc.dao;
+
+
+import java.util.List;
+
+import com.hibernate.Springmvc.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
